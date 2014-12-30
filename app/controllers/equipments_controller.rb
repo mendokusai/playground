962b3befc78f449ddb287f84484e4e312921078d
Ryan <ryanpauley@gmail.com>
2014-12-31 08:50:26 +1100

    Equipment CRUD complete, user/admin authorizations added

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -1,5 +1,49 @@
 class EquipmentsController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
+
 	def index
-		@equip = Equipment.all
+		@equips = Equipment.all
 	end
+
+	def new
+		@equip = Equipment.new
+	end
+
+	def create
+		@equip = Equipment.new(equip_params)
+		if @equip.save
+			redirect_to equipment_path(@equip)
+		else
+			render :new
+		end
+	end
+
+	def show 
+		@equip = Equipment.find(params[:id])
+	end
+
+	def edit
+		@equip = Equipment.find(params[:id])
+	end
+
+	def update
+		@equip = Equipment.find(params[:id])
+		if @equip.update(equip_params)
+		else
+			render :edit
+		end
+		redirect_to @equip
+	end
+
+	def destroy
+		@equip = Equipment.find(params[:id])
+		@equip.destroy
+		redirect_to equipments_path
+	end
+
+	private
+		def equip_params
+			params.require(:equipment).permit(:name, :description, :age_range)
+		end
+
 end
